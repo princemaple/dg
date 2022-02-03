2c7c65289ff1a2460c1f405542cd141e64835995
Po Chen <chenpaul914@gmail.com>
2022-02-03 16:46:43 +1100

    lower

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DG.MixProject do
     [
       app: :dg,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
