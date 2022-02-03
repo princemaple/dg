4eb8f4cb96291ea704f5cee50b009bf9f3c95b11
Po Chen <chenpaul914@gmail.com>
2022-02-03 16:34:56 +1100

    lower version requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DG.MixProject do
     [
       app: :dg,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
