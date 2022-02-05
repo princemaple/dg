a26ff5559d8e617b1435dd02b97e70d6c3396269
Po Chen <chenpaul914@gmail.com>
2022-02-06 10:23:33 +1100

    support label

--- lib/sigil.ex
+++ lib/sigil.ex
@@ -2,23 +2,39 @@ defmodule DG.Sigil do
   use AbnfParsec,
     abnf: """
     wsp = %x20 / %x09
-    lwsp = *(wsp / LF / CR LF)
+    lwsp = *(wsp / LF / CRLF)
     direction = "TB" / "TD" / "LR"
     type = "flowchart" / "graph"
-    vertex = 1*(ALPHA / DIGIT)
-    label = 1*(ALPHA / DIGIT / wsp)
+    label = 1*(ALPHA / DIGIT / %x20)
+    square-brace-open = "["
+    square-brace-close = "]"
+    vertex-id = 1*(ALPHA / DIGIT)
+    vertex = vertex-id [square-brace-open label square-brace-close]
     edge = vertex *wsp ("-->" / "--" label "-->") *wsp vertex
     vertex-or-edge = [lwsp] (edge / vertex)
     graph = lwsp type 1*wsp direction lwsp *vertex-or-edge
     """,
     parse: :graph,
-    unbox: ["vertex-or-edge"],
-    ignore: ["wsp", "lwsp"],
-    unwrap: ["type", "direction", "vertex", "label"],
+    unbox: ["vertex-or-edge", "vertex-id"],
+    ignore: ["wsp", "lwsp", "square-brace-open", "square-brace-close"],
+    unwrap: ["type", "direction", "label"],
     transform: %{
-      "vertex" => {:reduce, {List, :to_string, []}},
+      "vertex-id" => {:reduce, {List, :to_string, []}},
       "label" => {:reduce, {List, :to_string, []}}
-    }
+    },
+    debug: true
+
+  defp unwrap_vertex({:vertex, [v]}) do
+    {:vertex, v}
+  end
+
+  defp unwrap_vertex({:vertex, [v, label: label]}) do
+    {:vertex, v, label}
+  end
+
+  defp extract_vertex({:vertex, [v | _]}) do
+    v
+  end
 
   defmacro sigil_g({:<<>>, _, [string]}, _opts) do
     {:ok, [graph: [{:type, _type}, {:direction, direction} | content]], _, _, _, _} =
@@ -26,41 +42,30 @@ defmodule DG.Sigil do
 
     vertices =
       content
-      |> Enum.filter(fn
-        {:vertex, _v} -> true
-        {:vertex, _v, _label} -> true
-        {:edge, [_v1, "-->", _v2]} -> true
-        {:edge, [_v1, "--", _label, "-->", _v2]} -> true
-        _ -> false
-      end)
       |> Enum.flat_map(fn
-        {:vertex, v} ->
-          [{:vertex, v}]
+        {:vertex, _} = v ->
+          [unwrap_vertex(v)]
 
-        {:vertex, v, label} ->
-          [{:vertex, v, label}]
+        {:edge, [v1, "-->", v2]} ->
+          [unwrap_vertex(v1), unwrap_vertex(v2)]
 
-        {:edge, [{:vertex, v1}, "-->", {:vertex, v2}]} ->
-          [{:vertex, v1}, {:vertex, v2}]
-
-        {:edge, [{:vertex, v1}, "--", _label, "-->", {:vertex, v2}]} ->
-          [{:vertex, v1}, {:vertex, v2}]
+        {:edge, [v1, "--", _label, "-->", v2]} ->
+          [unwrap_vertex(v1), unwrap_vertex(v2)]
       end)
       |> Macro.escape()
 
     edges =
       content
       |> Enum.filter(fn
-        {:edge, [_v1, "-->", _v2]} -> true
-        {:edge, [_v1, "--", _label, "-->", _v2]} -> true
+        {:edge, _} -> true
         _ -> false
       end)
       |> Enum.map(fn
-        {:edge, [{:vertex, v1}, "-->", {:vertex, v2}]} ->
-          {:edge, v1, v2}
+        {:edge, [v1, "-->", v2]} ->
+          {:edge, extract_vertex(v1), extract_vertex(v2)}
 
-        {:edge, [{:vertex, v1}, "--", {:label, label}, "-->", {:vertex, v2}]} ->
-          {:edge, v1, v2, label}
+        {:edge, [v1, "--", {:label, label}, "-->", v2]} ->
+          {:edge, extract_vertex(v1), extract_vertex(v2), label}
       end)
       |> Macro.escape()
 
